50c11aba7463981b214ba03b54f1abc93503f3e6
Machaieie <machaieieedwin1@gmail.com>
2024-05-05 23:21:51 +0200

    atualizacoes

---
+++ lib/screens/reservas/reservas.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Reservas extends StatefulWidget {
+  const Reservas({super.key});
+
+  @override
+  State<Reservas> createState() => _ReservasState();
+}
+
+class _ReservasState extends State<Reservas> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
